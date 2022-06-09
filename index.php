3d393c0eea31258487a50d99d0896ff8dbdf80e2
Florexianer <8111@htl.rennweg.at>
2022-06-09 13:53:22 +0200

    changed database and insert now working

--- index.php
+++ index.php
@@ -4,12 +4,18 @@ header("Access-Control-Allow-Origin: '*'");
 
 
 if(isset($_POST)) {
+
     $response = json_decode(file_get_contents('php://input'),true);
     $db = new PDO("sqlite:" . __DIR__ . "/pushUp");
-    $query = $db->prepare("INSERT INTO 
-    Transformation(before, after)
-    VALUES (".$response['before'].",".$response['after'].")");
+    /*$query = $db->prepare("
+INSERT INTO Transformation(before, after) VALUES (?,?)");
     echo 'heeeeeeey';
-    echo $query;
-    $result = $query->execute();
+    $result = $query->execute([$response['before'],$response['after']]);*/
+
+    $query = $db->prepare("
+UPDATE User
+SET before = ?, after = ?
+WHERE pk_username = ?");
+    echo 'heeeeeeey';
+    $result = $query->execute([$response['before'],$response['after'],$response['username']]);
 }
